a4e95056cd82a890e138d81678deefd487847470
Claire Brownell <89938185+ClaireBrownell@users.noreply.github.com>
2024-04-25 09:08:12 -0400

    Update views.py

--- Django/views.py
+++ Django/views.py
@@ -1,6 +1,8 @@
 # dashboard/views.py
 from django.shortcuts import render, redirect
+from .models import Entry
 from .forms import UserRegistrationForm
+from django.contrib.auth.decorators import login_required
 from django.http import  HttpResponse
 
 def index(request):
@@ -23,3 +25,18 @@ def register(request):
     else:
         form = UserRegistrationForm()
     return render(request, 'registration/register.html', {'form': form})
+
+
+def diary_view(request):
+    entries = Entry.objects.filter(user=request.user).order_by('-created_at')
+    return render(request, 'diary.html', {'entries': entries})
+
+@login_required
+def add_entry_view(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        content = request.POST.get('content')
+        entry = Entry(user=request.user, title=title, content=content)
+        entry.save()
+        return redirect('diary_view')
+    return render(request, 'writing.html')
