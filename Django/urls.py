376acdd961c76a84f55022ca3782ff8e940a61fa
Claire Brownell <89938185+ClaireBrownell@users.noreply.github.com>
2024-04-25 09:39:29 -0400

    Delete Django/urls.py

--- Django/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import index, register, diary_view, add_entry_view
-
-urlpatterns = [
-    path('', index, name='index'),
-    path('register/', register, name='register'),
-    path('diary/', diary_view, name='diary_view'),
-    path('entry/add/', add_entry_view, name='add_entry_view'),
-]
